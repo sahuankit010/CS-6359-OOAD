daba5ee4817eb8275ef8dfce876ed741a361d500
Ankit Sahu <sahuankit010@Ankits-MacBook-Pro.local>
2022-06-28 20:05:04 -0500

    Initializer

---
+++ OO_Demo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int a,b;
+        a=4;
+        b=2;
+        System.out.println("The value of a = " + a);
+        System.out.println("The value of b = " + b);
+
+        System.out.println("Addition of a and b is "+ (a+b));
+        System.out.println("Subtraction of b from a is "+ (a-b));
+        System.out.println("Multiplication of a and b = " + a*b);
+        System.out.println("Division of a by a = " + a/b);
+    }
+}
